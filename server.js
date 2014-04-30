679e916b21818049079d59683148adb1cf8513e3
krasevych <roman.krasevyh>
2014-04-30 21:13:43 +0300

    update

--- server.js
+++ server.js
@@ -152,6 +152,8 @@ var SampleApp = function() {
 		    res.setHeader('Content-Type', 'text/html');
 		    res.send(self.cache_get('index.html') );
 	    });
+	    app.use(express.static(path.join(__dirname, 'public')));
+
 	    http.createServer(app).listen(8080,process.env.OPENSHIFT_NODEJS_IP, function () {
 		    console.log('Express server listening on port ');
 	    });/*
