052663c5a481e40dc3f54ea485dddc7862615641
krasevych <roman.krasevyh>
2014-04-30 18:17:36 +0300

    update

--- app.js
+++ app.js
@@ -51,6 +51,6 @@ app.use(function (err, reg, res, next) {
 });
 
 
-http.createServer(app).listen(config.get('port'), function () {
+http.createServer(app).listen(config.get('port'),'127.0.0.1', function () {
 	console.log('Express server listening on port ' + config.get('port'));
 });
