6ee9c804f2975c8ed128a0dae0ec524cf10fa3b8
krasevych <roman.krasevyh>
2014-04-30 20:40:39 +0300

    update

--- server.js
+++ server.js
@@ -146,7 +146,7 @@ var SampleApp = function() {
                         Date(Date.now() ), self.ipaddress, self.port);
         });*/
 
-	    self.app.listen(8080,process.env.OPENSHIFT_NODEJS_IP, function () {
+	    self.app.listen(8080,'ex-std-node413.prod.rhcloud.com', function () {
 		    console.log('Express server listening on port ');
 	    });
 	/*    for (var r in self.routes) {
