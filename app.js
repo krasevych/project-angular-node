f9c0c88d7080b486434aa5a36fc14bc9f26bb18d
krasevych <roman.krasevyh>
2014-04-30 22:59:54 +0300

    update

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var mongoose = require('./lib/mongoose');
+//var mongoose = require('./lib/mongoose');
 var http = require('http');
 var path = require('path');
 var config = require('./config');
