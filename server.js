e940899345828517a4956705d4c9c499b1bc574a
krasevych <roman.krasevyh>
2014-04-30 19:55:20 +0300

    update

--- server.js
+++ server.js
@@ -145,9 +145,8 @@ var SampleApp = function() {
             console.log('%s: Node server started on %s:%d ...',
                         Date(Date.now() ), self.ipaddress, self.port);
         });*/
-//	    var http = require('http');
 
-	    express.createServer().listen(8080,'ex-std-node413.prod.rhcloud.com', function () {
+	    self.app.listen(8080,'ex-std-node413.prod.rhcloud.com', function () {
 		    console.log('Express server listening on port ');
 	    });
 	/*    for (var r in self.routes) {
